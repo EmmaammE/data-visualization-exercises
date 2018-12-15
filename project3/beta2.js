0d3ee0c2d8b163b939085312a96f83a6b2ecbd49
zhuzhu <zrcccrz@163.com>
2018-12-15 21:50:07 +0800

    基本完成

--- project3/beta2.js
+++ project3/beta2.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   var template = {
     width: '1200',
     height: '520',
@@ -25,6 +25,9 @@
   var area, $path;
   var series;
   var brush;
+  var _scale = 1;
+  var toUpdate = false;
+  var updateIndex ;//存储area显示的颜色的次序
 
   var showLegend, legends;
   var $chart = d3
@@ -35,7 +38,7 @@
 
   function loadData(file) {
     return new Promise((resolve, reject) => {
-      d3.csv(file, function(error, data) {
+      d3.csv(file, function (error, data) {
         if (error) {
           reject(error);
         } else {
@@ -45,6 +48,10 @@
           showLegend = new Array(keys.length);
           showLegend.fill(1, 0, 10);
           showLegend.fill(0, 10);
+          updateIndex =[];
+          for (let index = 0; index < 10; index++) {
+            updateIndex.push(index)
+          }
           // showLegend.fill(1)
           // console.log(showLegend);
           resolve(data);
@@ -52,6 +59,7 @@
       });
     });
   }
+
   function get_colors(n) {
     var colors = [
       '#cab2d6',
@@ -65,14 +73,15 @@
       '#1d5464',
       '#207e82',
       '#1f78b4',
-      '#a6cee3',
-      '#ff7f00',
-      '#ebff66',
-      '#d8d860'
+      // '#a6cee3',
+      // '#ff7f00',
+      // '#ebff66',
+      // '#d8d860'
     ];
 
     return colors[n % colors.length];
   }
+
   function update(data) {
     // update yScale:
     inityScale(data);
@@ -88,6 +97,8 @@
       .order(d3.stackOrderAescending)
       .keys(keys_);
     series = stack(data);
+
+    toUpdate = true;
   }
 
   function inityScale(dataset) {
@@ -124,7 +135,7 @@
     xScale = d3
       .scaleLinear()
       .domain([1, dataset[dataset.length - 1]['chapter']])
-      .range([template.padding, template.width -  2*template.padding]);
+      .range([template.padding, template.width - 2 * template.padding]);
 
     xAxis = d3
       .axisBottom()
@@ -150,30 +161,36 @@
 
     area = d3
       .area()
-      .x(function(d) {
+      .x(function (d) {
         return xScale(d.data.chapter);
       })
-      .y0(function(d) {
+      .y0(function (d) {
         return yScale(d[0]);
       })
-      .y1(function(d) {
+      .y1(function (d) {
         return yScale(d[1]);
       })
-      .curve(d3.curveCatmullRom.alpha(0.5));
+      .curve(d3.curveBasis)
+      // .curve(d3.curveCatmullRom.alpha(0.5));
 
     renderAllAreas(data);
     renderAllLegends();
 
+    $chart.append("rect").attr("width", 2 * template.padding).attr('height', template.height + 2 - template.padding)
+      .attr('fill', '#fff')
+    $chart.append("rect").attr("width", template.padding).attr('height', template.height + 2 - template.padding)
+      .attr('transform', 'translate(' + (template.width - template.padding) + ', 0)')
+      .attr('fill', '#fff')
     $ele
       .append('g')
       .attr('class', 'x axis')
       .attr(
         'transform',
         'translate( ' +
-          template.padding +
-          ',' +
-          (template.height - template.padding) +
-          ')'
+        template.padding +
+        ',' +
+        (template.height - template.padding) +
+        ')'
       )
       .call(xAxis);
 
@@ -182,11 +199,16 @@
       .attr('class', 'y axis')
       .attr('transform', 'translate(' + template.padding * 2 + ',0)')
       .call(yAxis);
+
+
   }
 
   function renderAllAreas(data) {
     if ($path === undefined) {
-      $path = $chart
+      // $chart.append("g")
+      //   .attr("class","areas");
+      $path = $chart.append("g")
+        .attr("class", "areas")
         .selectAll('g')
         .data(series)
         .enter()
@@ -197,7 +219,11 @@
     } else {
       var $path_ = $chart.selectAll('g.area').data([]);
       $path_.exit().remove();
-      $path = $path_
+      console.log("---",$path);
+      $path =
+      // $path_
+      $path.select('g.areas')
+      // $path_
         .data(series)
         .enter()
         .append('g')
@@ -208,26 +234,31 @@
   }
 
   function renderArea($ele, data) {
+    console.log(updateIndex);
     let t = d3
       .transition()
-      //   .duration(1000)
-      //   .delay((d, i) => 200 * i)
+      .duration(1000)
+      .delay((d, i) => 200 * i)
       .ease();
 
+      console.log(showLegend);
     $ele
       .append('path')
+      .transition(t)
       .attr('d', area)
-      .transition()
-      .attr('fill', function(d, i) {
+      .attr('fill', function (d, i) {
+        if (toUpdate===true) {
+          return get_colors(updateIndex[i])
+        }
         return get_colors(i);
       });
 
     $ele
       .selectAll('path')
       .attr('class', (d, i) => i)
-      .attr('transform', 'translate(' + template.padding  + ',0)')
+      .attr('transform', 'translate(' + template.padding + ',0)')
       .attr('title', d => d.key)
-      .on('mouseover', function(d, i) {
+      .on('mouseover', function (d, i) {
         // 修改颜色
         $ele
           .select('path')
@@ -236,22 +267,29 @@
 
         d3.select(this)
           .attr('fill', () => {
-            return get_colors(i);
+             if (toUpdate===true) {
+               return get_colors(updateIndex[i])
+             }
+             return get_colors(i);
           })
           .attr('opacity', '1');
 
         // 展示章节
         let cor = d3.mouse(this);
+        console.log(_scale);
         let chapter_ = Math.round(
-          cor[0] / ((template.width - template.padding) / 120)
+          xScale.domain()[0] + cor[0] / ( (template.width - template.padding) / (xScale.domain()[1] - xScale.domain()[0]))
         );
         showLegendChapter(chapter_, data);
       })
-      .on('mouseout', function() {
+      .on('mouseout', function () {
         $ele
           .select('path')
-          .attr('fill', function(d, i) {
-            return get_colors(i);
+          .attr('fill', function (d, i) {
+             if (toUpdate===true) {
+               return get_colors(updateIndex[i])
+             }
+             return get_colors(i);
           })
           .attr('opacity', '1');
       })
@@ -287,47 +325,47 @@
   function renderAllLegends(chapterLegend, data) {
     if (chapterLegend !== undefined) {
 
-        var legend;
+      var legend;
 
+      legend = d3
+        .select('.legend')
+        .data([])
+        .exit()
+        .remove();
+
+      setTimeout(() => {
         legend = d3
-            .select('.legend')
-            .data([])
-            .exit()
-            .remove();
+          .select('#legend')
+          .append('div')
+          .attr('class', 'legend');
 
-            setTimeout(() => {
-                legend = d3
-                    .select('#legend')
-                    .append('div')
-                    .attr('class', 'legend');
+        legends = legend
+          .selectAll('div')
+          .data(chapterLegend)
+          .enter()
+          .append('span')
+          .attr('id', (d, i) => d[2]);
 
-                legends = legend
-                    .selectAll('div')
-                    .data(chapterLegend)
-                    .enter()
-                    .append('span')
-                    .attr('id', (d, i) => d[2]);
+        legends
+          .append('p')
+          .attr('class', 'number')
+          .text(d => d[1]);
+        console.log('-------\n', chapterLegend);
+        legends
+          .append('div')
+          .attr('class', 'icon')
+          .style('background-color', function (d) {
+            if (d[3] === true || d[3] === 1) {
+              console.log(d[3]);
+              return get_colors(d[2]-1);
+            } else {
+              return '#999';
+            }
+          });
 
-                legends
-                    .append('p')
-                    .attr('class', 'number')
-                    .text(d => d[1]);
-                console.log('-------\n', chapterLegend);
-                legends
-                    .append('div')
-                    .attr('class', 'icon')
-                    .style('background-color', function (d) {
-                        if (d[3] === true || d[3] === 1) {
-                            console.log(d[3]);
-                            return get_colors(d[2]-1);
-                        } else {
-                            return '#999';
-                        }
-                    });
-
-                legends.append('p').text(d => d[0]);
-                onClickLegends(data, 2);
-            }, 0);
+        legends.append('p').text(d => d[0]);
+        onClickLegends(data, 2);
+      }, 0);
     }
 
     if (legends === undefined) {
@@ -346,7 +384,7 @@
       legends
         .append('div')
         .attr('class', 'icon')
-        .style('background-color', function(d, i) {
+        .style('background-color', function (d, i) {
           if (showLegend[i]) {
             return get_colors(i);
           } else {
@@ -358,9 +396,11 @@
       // console.log('show undefined');
     } else {
       let showOrder = 0;
-      legends.selectAll('.icon').style('background-color', function() {
+      legends.selectAll('.icon').style('background-color', function () {
         if (showLegend[showOrder]) {
-          return get_colors(showOrder++);
+          let temp = showOrder;
+          showOrder++;
+          return get_colors(temp);
         } else {
           showOrder++;
           return '#999';
@@ -398,9 +438,12 @@
 
             let i = keys_.indexOf(keys[index]);
             if (i === -1) {
+              console.log(keys_);
               keys_.push(keys[index]);
+              updateIndex.push(+index);
             } else {
               keys_.splice(i, 1);
+              updateIndex.splice(i,1)
             }
             update(data);
             renderAllAreas(data);
@@ -432,7 +475,7 @@
       legends
         .append('div')
         .attr('class', 'icon')
-        .style('background-color', function(d, i) {
+        .style('background-color', function (d, i) {
           if (showLegend[i]) {
             return get_colors(i);
           } else {
@@ -454,11 +497,16 @@
       let temp = 0;
       let innerArray = Object.entries(inner);
       for (let index = 1; index < innerArray.length; index++) {
-        const value = innerArray[index][1];
-        parseInt(value) > 0 && (temp += +value);
+      //  if (showLegend[index-1]) {
+          const value = innerArray[index][1];
+          parseInt(value) > 0 && (temp += +value);
+      //  }
       }
       // console.log(inner);
-      sum.push({ chapter: parseInt(inner.chapter), value: temp });
+      sum.push({
+        chapter: parseInt(inner.chapter),
+        value: temp
+      });
     });
     console.log(sum);
     // define scale & axis
@@ -493,10 +541,10 @@
       .attr(
         'transform',
         'translate( ' +
-          timeline.padding +
-          ',' +
-          (timeline.height - timeline.padding) +
-          ')'
+        timeline.padding +
+        ',' +
+        (timeline.height - timeline.padding) +
+        ')'
       )
       .call(xAxis_);
 
@@ -508,15 +556,16 @@
 
     let areaT = d3
       .area()
-      .x(function(d, i) {
+      .x(function (d, i) {
         // console.log(d);
         return xScale_(d.chapter);
       })
-      .y1(function(d) {
+      .y1(function (d) {
         return yScale_(d.value);
       })
       .y0(yScale_(0))
-      .curve(d3.curveCatmullRom.alpha(0.5));
+      // .curve(d3.curveCatmullRom.alpha(0.1));
+      .curve(d3.curveNatural)
 
     var line = d3
       .line()
@@ -537,17 +586,20 @@
 
     brush = d3
       .brushX()
-      .extent([[xScale_(1), 0], [timeline.width, timeline.height]])
+      .extent([
+        [xScale_(1), 0],
+        [timeline.width, timeline.height]
+      ])
       .on('brush', brushed);
 
     // $timeline.append("g")
     //     .attr("class", "brush")
     //     .call(d3.brush().extent([timeline.padding, timeline.padding], [timeline.width / 2, timeline.height / 2]).on("brush", brushed));
-    // $timeline
-    //   .append('g')
-    //   .attr('class', 'brush')
-    //   .call(brush)
-    //   .call(brush.move, xScale_.range().map(value => value / 2));
+    $timeline
+      .append('g')
+      .attr('class', 'brush')
+      .call(brush)
+      .call(brush.move, xScale_.range().map(value => value / 2));
     // console.log(xScale_.range());
   }
 
@@ -556,26 +608,41 @@
   // }
 
   function brushed() {
-    var debug = true;
+    var debug = false;
     debug && console.log('brush');
     if (d3.event.sourceEvent && d3.event.sourceEvent.type === 'zoom') return; // ignore brush-by-zoom
     var s = d3.event.selection || xScale_.range();
     debug && console.log(s.map(xScale_.invert, xScale_));
-    var smap = s.map(xScale_.invert,xScale_);
-    // smap[0]+=template.padding;
-    // smap[1]-=template.padding;
+    var smap = s.map(xScale_.invert, xScale_);
+    if (smap[0] < 0) {
+      let helper = 0 - smap[0];
+      smap[0] = 0;
+      smap[1] += helper;
+    }
+    if (smap[1] > 120) {
+      smap[0] -= (smap[1] - 120);
+      smap[1] = 120;
+    }
+    let t = d3.transition().ease(d3.easePolyOut)
+    $chart.select('.x').transition(t);
     xScale.domain(smap).nice();
     debug && console.log(xScale.domain());
 
+    // console.log(brush.extent());
+    _scale = 2;
     xAxis = d3
       .axisBottom()
       .scale(xScale)
       .ticks(template.xScaleTick);
+      // let t = d3.transition
     $chart
       .selectAll('g.area')
       .select('path')
+      .transition(t)
       .attr('d', area)
-      .attr('transform', 'translate(' + 55 + ',0)');
+      .attr('transform', 'translate(' + template.padding + ',0)');
+
+
     $chart.select('g.x').call(xAxis);
   }
 
@@ -585,8 +652,6 @@
       render($chart, parsed_data);
       onClickLegends(parsed_data);
       onClickAll(parsed_data);
-      // renderbrush();
-
       renderTimeline(parsed_data);
     })
     .catch(error => {
