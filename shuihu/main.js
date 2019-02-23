467fc08b01ac8cf9f920069868308ec1d13048d7
zhuzhu <zrcccrz@163.com>
2019-02-23 21:55:17 +0800

    add tooltip&avoid nodes going outside

--- shuihu/main.js
+++ shuihu/main.js
@@ -1,4 +1,10 @@
-const BASIC_STRENGTH = 0.09
+const BASIC_STRENGTH = 0.09;
+const HEIGHT_FACTOR = 0.75;
+const STRENGTH = 20;
+const NODE_RADIUS = 10;
+const COLOR = {
+    nodeColor: '#9aa82d',
+}
 
 function smoothScroll(target, duration) {
     var target = document.querySelector(target);
@@ -51,18 +57,17 @@ function loadData(file) {
 function displayRelationNodes(data, type) {
     let width = window.innerWidth
     let height = window.innerHeight
+    let toRestart = 0;
     let svg = d3.select('svg')
         .attr('width', 0.9 * width)
         .attr('height', 1000)
 
-    const linkForce = d3.forceLink().id(link => link.name)
-        .distance(() => 100)
-        .strength(link => link.strength);
-
     // simulatin setup with all forces
     let simulation = d3.forceSimulation()
-        .force('charge', d3.forceManyBody().strength(-18))
-        .force('center', d3.forceCenter(width / 2, height * 0.75))
+        .force('charge', d3.forceManyBody().strength(-STRENGTH))
+        .force('center', d3.forceCenter(width / 2, height * HEIGHT_FACTOR))
+        // prevent nodes from ovelapping, treating them as circles with the given radius:
+        .force("collide", d3.forceCollide((NODE_RADIUS + 2) / 2))
 
     function getNodeColor(node) {
         return node.relation === '自己' ? 'red' : 'gray'
@@ -78,6 +83,12 @@ function displayRelationNodes(data, type) {
     }
 
     let links = createLinks();
+    const linkElements = svg.append('g')
+        .selectAll('line')
+        .data(links)
+        .enter().append('line')
+        .attr('stroke-width', 0.7)
+        .attr('stroke', '#E5E5E5')
 
     const nodeElements = svg.append('g')
         .selectAll('circle')
@@ -91,41 +102,37 @@ function displayRelationNodes(data, type) {
         .data(data)
         .enter().append('text')
         .text(node => node.name)
-        .attr('font-size', 10)
-        .attr('dx', 15)
-        .attr('dy', 4)
+        .attr('font-size', 12)
+        .attr('dx', 12)
+        .attr('dy', 6)
 
-    const nickNames = svg.append('g')
-        .selectAll('text')
-        .data(data)
-        .enter().append('text')
-        .text(node => node.nickname)
-        .attr('font-size', 10)
-        .attr('dx', 20)
-        .attr('dy', 18)
+    // const nickNames = svg.append('g')
+    //     .selectAll('text')
+    //     .data(data)
+    //     .enter().append('text')
+    //     .text(node => node.nickname)
+    //     .attr('font-size', 10)
+    //     .attr('dx', 20)
+    //     .attr('dy', 18)
 
-    const linkElements = svg.append('g')
-        .selectAll('line')
-        .data(links)
-        .enter().append('line')
-        .attr('stroke-width', 0.7)
-        .attr('stroke', '#E5E5E5')
+
 
     simulation.nodes(data).on('tick', () => {
-        nodeElements
-            .attr('cx', node => node.x)
-            .attr('cy', node => node.y)
-        textElements
-            .attr('x', node => node.x)
-            .attr('y', node => node.y)
-        nickNames
-            .attr('x', node => node.x)
-            .attr('y', node => node.y)
         linkElements
-            .attr('x1', link => link.source.x)
-            .attr('y1', link => link.source.y)
-            .attr('x2', link => link.target.x)
-            .attr('y2', link => link.target.y)
+            .attr('x1', link => getNodeXCoordinate(link.source.x + NODE_RADIUS))
+            .attr('y1', link => getNodeYCoordinate(link.source.y + NODE_RADIUS))
+            .attr('x2', link => getNodeXCoordinate(link.target.x + NODE_RADIUS))
+            .attr('y2', link => getNodeYCoordinate(link.target.y + NODE_RADIUS))
+        nodeElements
+            .attr('cx', node => getNodeXCoordinate(node.x))
+            .attr('cy', node => getNodeYCoordinate(node.y))
+        textElements
+            .attr('x', node => getNodeXCoordinate(node.x))
+            .attr('y', node => getNodeYCoordinate(node.y))
+        // nickNames
+        //     .attr('x', node => node.x)
+        //     .attr('y', node => node.y)
+
     })
 
     // for links
@@ -157,11 +164,14 @@ function displayRelationNodes(data, type) {
 
     const dragDrop = d3.drag()
         .on('start', node => {
-            node.fx = node.x
+            if (!d3.event.active) {
+                simulation.alphaTarget(0.9).restart();
+            }
+            node.fx = node.x;
             node.fy = node.y
         })
         .on('drag', node => {
-            simulation.alphaTarget(0.5).restart()
+            // console.log(node);
             node.fx = d3.event.x
             node.fy = d3.event.y
         })
@@ -173,7 +183,54 @@ function displayRelationNodes(data, type) {
             node.fy = null
         })
 
-    nodeElements.call(dragDrop)
+    nodeElements.call(dragDrop);
+
+    const tooltip = d3.select('#relation').append("div")
+        .classed("tooltip", true)
+        .style("opacity", 0);
+    nodeElements
+        .on("mouseover", function (d) {
+            tooltip.transition()
+                .duration(300)
+                .style("opacity", 1) // show the tooltip
+            tooltip.html(() => {
+                if (d.relation) {
+                    return d.relation + "\n" + d.nickname
+                } else {
+                    return d.relation
+                }
+            })
+                .style("left", (d3.event.pageX - d3.select('.tooltip').node().offsetWidth - 5) + "px")
+                .style("top", (d3.event.pageY - d3.select('.tooltip').node().offsetHeight) + "px");
+        })
+        .on("mouseleave", function (d) {
+            tooltip.transition()
+                .duration(200)
+                .style("opacity", 0)
+        })
+
+    // functions to avoid nodes and links going outside the svg container, when calculating the position:
+    function getNodeXCoordinate(x) {
+        // return Math.max(0, Math.min(width - NODE_RADIUS, x))
+        if (x<=0) {
+            x = NODE_RADIUS;
+        } else if (x>=0.9*width) {
+            x = 0.85*width
+        }
+        return x;
+        // return Math.max(0, Math.min(0.9 * width - NODE_RADIUS, x))
+    }
+
+    function getNodeYCoordinate(y) {
+        // return Math.max(0, Math.min(height - NODE_RADIUS, y))
+        // return Math.max(0, Math.min(900 - NODE_RADIUS, y))
+          if (y <= 0) {
+              y = NODE_RADIUS;
+          } else if (y >= 900) {
+              y = 850
+          }
+          return y;
+    }
 }
 
 loadData('./data/relation.csv')
