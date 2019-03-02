3b59b21557c22b77ba2b8ee3a0decd4486300835
zhuzhu <zrcccrz@163.com>
2019-03-02 20:16:47 +0800

    divide into groups

--- shuihu/main.js
+++ shuihu/main.js
@@ -3,7 +3,9 @@ const HEIGHT_FACTOR = 0.75;
 const STRENGTH = 20;
 const NODE_RADIUS = 10;
 const COLOR = {
-    nodeColor: '#9aa82d',
+    brotherColor: '#a3e6e9',
+    generalColor: '#6c5b7b',
+    goodColor: '#7795c5'
 }
 
 function smoothScroll(target, duration) {
@@ -54,14 +56,14 @@ function loadData(file) {
     });
 }
 
-function displayRelationNodes(data, type) {
-    let width = window.innerWidth
-    let height = window.innerHeight
-    let toRestart = 0;
+function displayRelationNodes(data, type, width_, height_) {
+    let width = width_ || window.innerWidth
+    let height = height_ || window.innerHeight
     let svg = d3.select('svg')
         .attr('width', 0.9 * width)
         .attr('height', 1000)
 
+    let selectedId;
     // simulatin setup with all forces
     let simulation = d3.forceSimulation()
         .force('charge', d3.forceManyBody().strength(-STRENGTH))
@@ -74,30 +76,39 @@ function displayRelationNodes(data, type) {
     }
 
     if (type === 2) {
-        // svg.selectAll('circle')
-        //     .data([])
-        //     .exit()
-        //     .remove();
-        // svg.selectAll('text').data([]).exit().remove();
         svg.selectAll('g').data([]).exit().remove();
     }
 
     let links = createLinks();
-    const linkElements = svg.append('g')
+
+    const linkGroup = svg.append("g").attr("class", "links")
+    const nodeGroup = svg.append("g").attr("class", "nodes")
+    const textGroup = svg.append("g").attr("class", "texts")
+
+    const linkElements = linkGroup
         .selectAll('line')
         .data(links)
         .enter().append('line')
         .attr('stroke-width', 0.7)
-        .attr('stroke', '#E5E5E5')
+        .attr('stroke', (d)=>{
+            if (d.relation === '兄弟') {
+                return COLOR.brotherColor
+            } else if (d.relation === '降将') {
+                return COLOR.generalColor
+            } else if (d.relation === '好兄弟') {
+                return COLOR.goodColor
+            }
+            return '#E5E5E5'
+        })
 
-    const nodeElements = svg.append('g')
+    const nodeElements = nodeGroup
         .selectAll('circle')
         .data(data)
         .enter().append('circle')
         .attr('r', 10)
         .attr('fill', getNodeColor)
 
-    const textElements = svg.append('g')
+    const textElements = textGroup
         .selectAll('text')
         .data(data)
         .enter().append('text')
@@ -115,8 +126,7 @@ function displayRelationNodes(data, type) {
     //     .attr('dx', 20)
     //     .attr('dy', 18)
 
-
-
+    // console.log(data,'before');
     simulation.nodes(data).on('tick', () => {
         linkElements
             .attr('x1', link => getNodeXCoordinate(link.source.x + NODE_RADIUS))
@@ -154,6 +164,7 @@ function displayRelationNodes(data, type) {
                     link.target = ele.group;
                     link.source = ele.name;
                     link.strength = BASIC_STRENGTH;
+                    link.relation = ele.relation;
                     links.push(link)
                 }
             }
@@ -161,6 +172,7 @@ function displayRelationNodes(data, type) {
         console.log(links);
         return links;
     }
+    // ---checked
 
     const dragDrop = d3.drag()
         .on('start', node => {
@@ -183,25 +195,28 @@ function displayRelationNodes(data, type) {
             node.fy = null
         })
 
-    nodeElements.call(dragDrop);
+    nodeElements.call(dragDrop)
+    // .on('click',selectNode);
 
     const tooltip = d3.select('#relation').append("div")
         .classed("tooltip", true)
         .style("opacity", 0);
     nodeElements
         .on("mouseover", function (d) {
-            tooltip.transition()
-                .duration(300)
-                .style("opacity", 1) // show the tooltip
-            tooltip.html(() => {
-                if (d.relation) {
-                    return d.relation + "\n" + d.nickname
-                } else {
-                    return d.relation
-                }
-            })
-                .style("left", (d3.event.pageX - d3.select('.tooltip').node().offsetWidth - 5) + "px")
-                .style("top", (d3.event.pageY - d3.select('.tooltip').node().offsetHeight) + "px");
+            if (d.relation) {
+                tooltip.transition()
+                    .duration(300)
+                    .style("opacity", 1) // show the tooltip
+                tooltip.html(() => {
+                        if (d.relation === '自己') {
+                            return d.nickname;
+                        } else {
+                            return d.relation + "<br/>" + d.nickname;
+                        }
+                    })
+                    .style("left", (d3.event.pageX - d3.select('.tooltip').node().offsetWidth - 5) + "px")
+                    .style("top", (d3.event.pageY - d3.select('.tooltip').node().offsetHeight) + "px");
+            }
         })
         .on("mouseleave", function (d) {
             tooltip.transition()
@@ -209,42 +224,98 @@ function displayRelationNodes(data, type) {
                 .style("opacity", 0)
         })
 
+    let factor  = 0.1;
+    let h = 1;
     // functions to avoid nodes and links going outside the svg container, when calculating the position:
     function getNodeXCoordinate(x) {
-        // return Math.max(0, Math.min(width - NODE_RADIUS, x))
-        if (x<=0) {
-            x = NODE_RADIUS;
-        } else if (x>=0.9*width) {
-            x = 0.85*width
-        }
-        return x;
-        // return Math.max(0, Math.min(0.9 * width - NODE_RADIUS, x))
+        return Math.max(0, Math.min(0.9 * width - NODE_RADIUS, x))
     }
 
     function getNodeYCoordinate(y) {
-        // return Math.max(0, Math.min(height - NODE_RADIUS, y))
-        // return Math.max(0, Math.min(900 - NODE_RADIUS, y))
-          if (y <= 0) {
-              y = NODE_RADIUS;
-          } else if (y >= 900) {
-              y = 850
-          }
-          return y;
+        return Math.max(0, Math.min(900 - NODE_RADIUS, y))
+        // let y_ = Math.max(0, Math.min(900 - NODE_RADIUS, y));
+        // if (y_> 880) {
+        //     y_= y_ * factor * h;
+        //     h = (h + 1) % 9;
+        // }
+
+        // return  y_
     }
+
+    /**
+     * @requires after createLinks
+     * key:name
+     */
+    function getNeighbors(node) {
+        return links.reduce((neighbors, link) => {
+            if (link.target.name === node.name) {
+                neighbors.push(link.source.name)
+            } else if (link.source.name === node.name) {
+                neighbors.push(link.target.name)
+            }
+            return neighbors
+        }, [node.name])
+    }
+    function isNeighborLink(node, link) {
+        return link.target.name === node.name || link.source.name === node.name
+    }
+
+    return simulation;
+}
+
+function handleRelationData(data) {
+    let groups = {
+        "无党派": []
+    };
+    data.forEach(ele => {
+        let _group = ele.group;
+        if (_group === "无党派") {
+            groups["无党派"].push(ele);
+        } else if (groups.hasOwnProperty(_group)) {
+            groups[_group].push(ele)
+        } else {
+            groups[_group] = [ele];
+        }
+    });
+    // console.log(groups);
+    return groups;
 }
 
 loadData('./data/relation.csv')
     .then(data => {
         console.log(data);
-        /**
-         * relation data:
-         *
-         */
-        displayRelationNodes(data)
+        // 分组点击事件
+        let groups = handleRelationData(data);
+        let currentData = data;
+        // currentData = groups['无党派'];
+        console.log(groups);
+        document.getElementById('groups').addEventListener('click',function(event){
+            // let target = event.target;
+            // if (target.tagName != 'IMG') {
+            //     return;
+            // }
+            let $parent = event.target.closest('div');
+            if (!$parent) return;
+            if ($parent.className != 'item') return;
+            let group = $parent.dataset.group;
+            if (group) {
+                currentData = groups[group];
+                s = displayRelationNodes(currentData, 2, 1000);
+                s.alphaTarget(0.7).restart()
+            }
+        })
+        let s = displayRelationNodes(currentData, 1, 1000)
 
         window.addEventListener('resize', () => {
-            displayRelationNodes(data, 2)
+            s = displayRelationNodes(currentData, 2, 1000)
         }, false);
+
+        document.getElementById('fresh-button').onclick = function () {
+            // s.restart();
+            currentData = data
+            s = displayRelationNodes(currentData, 2, 1000)
+
+        }
     })
     .catch(error => {
         console.log(error);
