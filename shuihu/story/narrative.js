45ea706400b9649bd3d61ffc8d6a414ebd56b142
zhuzhu <zrcccrz@163.com>
2019-03-22 13:47:59 +0800

    story beta1

---
+++ shuihu/story/narrative.js
@@ -0,0 +1,8 @@
+// Narrative Charts
+// ================
+//
+// `d3.layout.narrative()`
+//
+// The constructor takes no arguements. All relevant object properties should
+// be set using the setter functions.
+
